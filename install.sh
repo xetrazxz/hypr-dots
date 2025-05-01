81cf423c0a34f3ca0ded542f3035eed532317834
xetrazxz <xetrazx@gmail.com>
2025-05-01 22:33:31 +0530

    x

--- install.sh
+++ install.sh
@@ -36,9 +36,7 @@ sudo tar -xvf /home/$ORIGINAL_USER/hypr-dots/assets/themes/Catppuccin-Mocha.tar.
 sudo tar -xvf /home/$ORIGINAL_USER/hypr-dots/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/
 sudo cp -r /home/$ORIGINAL_USER/hypr-dots/assets/BG/ /home/$ORIGINAL_USER/Pictures/
 sudo cp -r /home/$ORIGINAL_USER/hypr-dots/config/* /home/$ORIGINAL_USER/.config
-mkdir /home/$ORIGINAL_USER/Pictures/screenshots
 sudo chown "$ORIGINAL_USER:$ORIGINAL_USER" /home/$ORIGINALUSER/.config/*
-sudo chown /home/$ORIGINAL_USER/Pictures/screenshots
 #
 echo "DONE"
 echo "run 'nwg-look' and set GTK and Icon Theme"

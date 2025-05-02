d708f64f4582e0315af19edcb800958a746c0cdf
xetrazxz <xetra@archlinux.localdomain>
2025-05-02 18:34:59 +0530

    x

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ cd && rm -rf /home/$ORIGINAL_USER/yay
 # ---- Run pacman as root ----
 echo "Installing system packages..."
 sudo pacman -Syu
-sudo pacman -S --noconfirm git base-devel xdg-user-dirs hyprland swww hyprlock hypridle hyprpicker dunst waybar foot tar nvim hyprpolkitagent xdg-desktop-portal-hyprland qt5-wayland qt6-wayland cliphist pipewire wireplumber brightnessctl ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono nwg-look qt5ct qt6ct kvantum nautilus neofetch
+sudo pacman -S --noconfirm git base-devel xdg-user-dirs foot hyprland swww hyprlock hypridle hyprpicker dunst waybar foot tar nvim hyprpolkitagent xdg-desktop-portal-hyprland qt5-wayland qt6-wayland cliphist pipewire wireplumber brightnessctl ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono nwg-look qt5ct qt6ct kvantum nautilus neofetch
 sudo -u "$ORIGINAL_USER" xdg-user-dirs-update
 # ---- Run yay as original user (non-root) ----
 echo "Installing AUR packages with yay..."

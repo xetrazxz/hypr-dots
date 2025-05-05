8050cbf7cd9d4e8503caf41452cec91f76d0e1bd
xetra <xetrazx@gmail.com>
2025-05-06 01:22:38 +0530

    x

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 # ---- Run pacman as root ----
 echo "Installing system packages..."
 sudo pacman -Syu
-sudo pacman -S --noconfirm git base-devel xdg-user-dirs foot hyprland swww hyprlock hypridle hyprpicker dunst waybar foot tar nvim hyprpolkitagent xdg-desktop-portal-hyprland qt5-wayland qt6-wayland cliphist pipewire wireplumber brightnessctl ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono nwg-look qt5ct qt6ct kvantum nautilus fastfetch foot
+sudo pacman -S --noconfirm git base-devel xdg-user-dirs foot hyprland swww hyprlock hypridle hyprpicker dunst waybar fish tar nvim hyprpolkitagent xdg-desktop-portal-hyprland qt5-wayland qt6-wayland cliphist pipewire wireplumber brightnessctl ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono nwg-look qt5ct qt6ct kvantum nautilus fastfetch
 xdg-user-dirs-update
 # ---- Run yay as original user (non-root) ----
 echo "Installing AUR packages with yay..."
@@ -14,7 +14,4 @@ yay -S --noconfirm thorium-browser-bin tofi wlogout grimblast kvantum-theme-catp
 sudo tar -xvf /home/$ORIGINAL_USER/hypr-dots/assets/themes/Catppuccin-Mocha.tar.xz -C /usr/share/themes/
 sudo tar -xvf /home/$ORIGINAL_USER/hypr-dots/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/
 #
-echo "DONE"
-echo "run 'nwg-look' and set GTK and Icon Theme"
-echo "run 'sudo -E kvantummanager' and set Catppuccin Theme"
-echo "run 'qt6ct' to set icon theme"
+cp -r /home/$ORIGINAL_USER/hypr-dots/assets/config/* /home/$ORIGINAL_USER/.config/
